a1e99707b4baa55aba9735269dd17494836289a9
Ben Hilger <bhilger78@gmail.com>
2024-05-21 19:11:28 -0400

    add basic ios project

---
+++ ios/accountame/accountame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  accountame
+//
+//  Created by Benjamin Hilger on 5/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/accountame/accountameApp.swift
@@ -0,0 +1,17 @@
+//
+//  accountameApp.swift
+//  accountame
+//
+//  Created by Benjamin Hilger on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct accountameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
