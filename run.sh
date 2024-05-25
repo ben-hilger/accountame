4dbc669e3393c014ef9e0d14f506023a0462e177
Ben Hilger <bhilger78@gmail.com>
2024-05-25 12:15:51 -0400

    feat: add ngrok

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ if [[ "$ACTION" == "start" ]]; then
   docker-compose up --remove-orphans
 elif [[ "$ACTION" == "migrate" ]]; then
   sh database/local/migrate-local.sh
+elif [[ "$ACTION" == "ngrok" ]]; then
+  docker run --net=host -it -e NGROK_AUTHTOKEN="$NGROK_AUTHTOKEN" ngrok/ngrok http 8080
 else
   echo "Unsupported action $ACTION"
 fi
